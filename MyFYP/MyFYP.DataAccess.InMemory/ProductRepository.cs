a7d0187a72e0a2b253aeaa10c130c71565ed329b
enyamc <57432948+enyamc@users.noreply.github.com>
2019-11-16 12:40:21 +0000

    Product Manager Built and Tested

--- MyFYP/MyFYP.DataAccess.InMemory/ProductRepository.cs
+++ MyFYP/MyFYP.DataAccess.InMemory/ProductRepository.cs
@@ -9,7 +9,7 @@ using MyFYP.Core.Models;
 
 namespace MyFYP.DataAccess.InMemory
 {
-    class ProductRepository
+    public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
         List<Product> products;

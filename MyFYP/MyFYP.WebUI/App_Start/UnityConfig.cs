be9d5403547bb41603370ebe06a81d360b635fcc
enyamc <57432948+enyamc@users.noreply.github.com>
2019-11-24 14:59:40 +0000

    Added SQL connection

--- MyFYP/MyFYP.WebUI/App_Start/UnityConfig.cs
+++ MyFYP/MyFYP.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using MyFYP.Core.Contracts;
 using MyFYP.Core.Models;
 using MyFYP.DataAccess.InMemory;
+using MyFYP.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace MyFYP.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }

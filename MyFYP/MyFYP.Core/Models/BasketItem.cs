fd98af8976d491c3260f607ef42b73692ff56148
enyamc <57432948+enyamc@users.noreply.github.com>
2020-01-06 16:49:24 +0000

    Adding basket service

--- MyFYP/MyFYP.Core/Models/BasketItem.cs
+++ MyFYP/MyFYP.Core/Models/BasketItem.cs
@@ -12,5 +12,6 @@ namespace MyFYP.Core.Models
         public string BasketId { get; set; }
         public string ProductId { get; set; }
         public int Quantity { get; set; }
+        
     }
 }

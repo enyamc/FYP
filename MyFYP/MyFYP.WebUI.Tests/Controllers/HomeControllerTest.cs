aff5c9a0095e0506312e862e52681c2e3e6e6c75
enyamc <57432948+enyamc@users.noreply.github.com>
2020-01-08 18:41:27 +0000

    Added registration page and db table

--- MyFYP/MyFYP.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyFYP/MyFYP.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -15,40 +15,17 @@ namespace MyFYP.WebUI.Tests.Controllers
         [TestMethod]
         public void Index()
         {
-            //// Arrange
+            ////// Arrange
             //HomeController controller = new HomeController();
 
-            //// Act
+            ////// Act
             //ViewResult result = controller.Index() as ViewResult;
 
-            //// Assert
-            //Assert.IsNotNull(result);
+            ////// Assert
+            //Assert.IsTrue(1 == 1);
         }
 
-        [TestMethod]
-        public void About()
-        {
-            //// Arrange
-            //HomeController controller = new HomeController();
-
-            //// Act
-            //ViewResult result = controller.About() as ViewResult;
-
-            //// Assert
-            //Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            //// Arrange
-            //HomeController controller = new HomeController();
-
-            //// Act
-            //ViewResult result = controller.Contact() as ViewResult;
-
-            //// Assert
-            //Assert.IsNotNull(result);
-        }
     }
+
 }
+        

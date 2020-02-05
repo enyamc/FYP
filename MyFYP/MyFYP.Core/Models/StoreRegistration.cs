d1828ee76b15329135c0b4adb4dec73021c26300
enyamc <57432948+enyamc@users.noreply.github.com>
2020-02-05 14:45:20 +0000

    Starting store registration

--- MyFYP/MyFYP.Core/Models/StoreRegistration.cs
+++ MyFYP/MyFYP.Core/Models/StoreRegistration.cs
@@ -9,5 +9,13 @@ namespace MyFYP.Core.Models
     public class StoreRegistration : BaseEntity
     {
         public string StoreID { get; set; }
+        public string StoreName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Eircode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+   
     }
 }
